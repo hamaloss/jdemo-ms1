c96321ff8a97483c87d59fdada574e6e3d8c7284
Ossi Hämäläinen <ossi.hamalainen@tieto.com>
2019-10-03 17:36:30 +0300

    Initial commit

---
+++ src/main/java/fi/tieto/teco/demo/repository/CarRepository.java
@@ -0,0 +1,14 @@
+package fi.tieto.teco.demo.repository;
+import fi.tieto.teco.demo.domain.Car;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Car entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+}
